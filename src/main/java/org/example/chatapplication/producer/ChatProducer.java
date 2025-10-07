6a6a5ae94817d434396607316e190ca56d984619
TornovDutta <tornovdutta@gmail.com>
2025-10-07 17:47:20 +0530

    rest api

--- src/main/java/org/example/chatapplication/producer/ChatProducer.java
+++ src/main/java/org/example/chatapplication/producer/ChatProducer.java
@@ -1,7 +1,15 @@
 package org.example.chatapplication.producer;
 
-import org.springframework.stereotype.Service;
 
-@Service
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class ChatProducer {
+    @GetMapping("/chat")
+    public void chat(){
+
+    }
 }
