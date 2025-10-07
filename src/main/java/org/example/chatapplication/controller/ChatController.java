0553ee33a1254f1e0abd79cbea37b46dba70a71b
TornovDutta <tornovdutta@gmail.com>
2025-10-07 17:53:24 +0530

    chats rest api

--- src/main/java/org/example/chatapplication/controller/ChatController.java
+++ src/main/java/org/example/chatapplication/controller/ChatController.java
@@ -1,9 +1,21 @@
 package org.example.chatapplication.controller;
 
+import org.example.chatapplication.producer.ChatProducer;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/chat")
+@RequestMapping("/api")
 public class ChatController {
+    private final ChatProducer producer;
+
+    public ChatController(ChatProducer producer) {
+        this.producer = producer;
+    }
+    @GetMapping("chat")
+    public String chat(){
+        return null;
+    }
+
 }
--- src/main/java/org/example/chatapplication/producer/ChatProducer.java
+++ src/main/java/org/example/chatapplication/producer/ChatProducer.java
@@ -1,15 +1,8 @@
 package org.example.chatapplication.producer;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api")
+import org.springframework.stereotype.Service;
+@Service
 public class ChatProducer {
-    @GetMapping("/chat")
-    public void chat(){
 
-    }
 }
